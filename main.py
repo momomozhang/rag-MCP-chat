810e9ffd3991d54437e5214190151e88157a5f83
Mengni Zhang <monicamengni@gmail.com>
2025-06-03 17:24:11 +0200

    add chat history, so the ai can remember the previous queries

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ from typing import Set
 
 import streamlit as st
 from dotenv import load_dotenv
-from streamlit_chat import message
 
 from backend.rag_openai import run_llm
 
 load_dotenv()
 
-st.header("MCP - Documentation Chatbot")
+st.header("MCP RAG Chatbot")
 
 prompt = st.text_input(
     "Prompt",
@@ -20,6 +19,9 @@ if "user_prompt_history" not in st.session_state:
 
 if "chat_answers_history" not in st.session_state:
     st.session_state["chat_answers_history"] = []
+
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
 
 
 def create_sources_string(source_urls: Set[str]) -> str:
@@ -44,7 +46,10 @@ def create_sources_string(source_urls: Set[str]) -> str:
 
 if prompt:
     with st.spinner("Generating response..."):
-        generated_response = run_llm(query=prompt)
+        generated_response = run_llm(
+            query=prompt,
+            chat_history=st.session_state["chat_history"],
+        )
         sources = set([doc.metadata["source"] for doc in generated_response["context"]])
 
         formatted_response = (
@@ -53,6 +58,8 @@ if prompt:
 
         st.session_state["user_prompt_history"].append(prompt)
         st.session_state["chat_answers_history"].append(formatted_response)
+        st.session_state["chat_history"].append(("human", prompt))
+        st.session_state["chat_history"].append(("ai", generated_response["answer"]))
 
 if st.session_state["chat_answers_history"]:
     for generated_response, user_query in zip(
